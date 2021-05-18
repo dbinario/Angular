365b61f13d0200a1e3149d1867b536bd901a5c24
Fernando Ruiz <fruiz@wellcom.com.mx>
2021-05-18 16:56:10 -0500

    tipos basicos

---
+++ ts-intro/src/ejercicios/01-tipos-basicos.ts
@@ -0,0 +1,19 @@
+
+/*
+    ===== Código de TypeScript =====
+*/
+//constante
+const nom='Fer';
+//variable tipo string
+let nombre: string ='Fernando';
+//variable numer o string
+let hp: number | string=95;
+//variable booleana
+let estaVivo: boolean=false;
+
+
+hp='FULL';
+
+nombre='Fernando';
+
+console.log(nombre,hp,estaVivo);
