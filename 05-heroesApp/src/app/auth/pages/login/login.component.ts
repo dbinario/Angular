6f77137872cc7232155157a2048ede70ddfd4176
Fernando Ruiz <fruiz@wellcom.com.mx>
2022-03-13 23:24:53 -0600

    se iniicia la seccion de proteccion de rutas

--- 05-heroesApp/src/app/auth/pages/login/login.component.ts
+++ 05-heroesApp/src/app/auth/pages/login/login.component.ts
@@ -1,14 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../service/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html'
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent  {
 
-  constructor() { }
+  constructor(private router: Router,
+              private authService:AuthService) { }
 
-  ngOnInit(): void {
+  login(){
+    
+    //ir al backend
+    //si el usuario existe
+
+    this.authService.login()
+    .subscribe(
+      resp=>{
+        console.log(resp);
+
+        if(resp.id){
+        
+          this.router.navigate(['./heroes']);
+          
+        }
+
+      }
+    );
+
+    
+
+
   }
 
 }
